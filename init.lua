ba749abddb699143ff6706aaf337ddfb590afd27
Ege Güneş <ege.gunes@percona.com>
2024-07-24 22:38:37 +0300

    run goimports on save

--- init.lua
+++ init.lua
@@ -658,6 +658,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'goimports', 'gofmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
