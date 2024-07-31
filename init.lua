841dfe9a5cacb9ec9ee40fa7ab450bd611d65cfb
Ege Güneş <ege.gunes@percona.com>
2024-07-31 11:39:23 +0300

    add ruifm/gitlinker.nvim

--- init.lua
+++ init.lua
@@ -273,6 +273,16 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'ruifm/gitlinker.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('gitlinker').setup {
+        mappings = '<leader>og',
+      }
+    end,
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
