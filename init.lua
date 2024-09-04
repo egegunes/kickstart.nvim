1c3b0ab1a2ac81baa40cd98d24f5d97468f01d1b
Ege Günes <egegunes@gmail.com>
2024-09-04 11:27:03 +0300

    add live_grep_args and keymaps

--- init.lua
+++ init.lua
@@ -364,6 +364,13 @@ require('lazy').setup({
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+
+      {
+        'nvim-telescope/telescope-live-grep-args.nvim',
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = '^1.0.0',
+      },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -401,20 +408,30 @@ require('lazy').setup({
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
+          live_grep_args = {
+            -- override default mapping to prompt for custom directory
+            auto_quoting = true, -- enable quoting in the prompt for more complex args
+            mappings = {
+              -- customize mappings to set search dir
+              i = {
+                ['<C-d>'] = require('telescope-live-grep-args.actions').grep_dir, -- press Ctrl+d to select a custom directory
+              },
+            },
+          },
         },
       }
 
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'live_grep_args')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      local extensions = require('telescope').extensions
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -425,6 +442,21 @@ require('lazy').setup({
           sort_lastused = true,
         }
       end, { desc = '[ ] Find existing buffers' })
+
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set(
+        'n',
+        '<leader>sgd',
+        ":lua require('telescope').extensions.live_grep_args.live_grep_args({ cwd = vim.fn.input('Search Dir: ', '', 'dir') })<CR>",
+        { desc = '[S]earch by [G]rep in [D]irectory', noremap = true, silent = true }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>sgb',
+        ":lua require('telescope').extensions.live_grep_args.live_grep_args({ cwd = vim.fn.expand('%:p:h') })<CR>",
+        { noremap = true, silent = true }
+      )
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
